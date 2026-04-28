14a8d3ec2aad54882dc2a5b7b13affe886002161
MicaellaBF212 <micaellabustos5@gmail.com>
2026-04-27 22:05:33 -0500

    actualizacion

--- src/App.java
+++ src/App.java
@@ -1,6 +1,40 @@
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+import controllers.SortMethods;
+import models.Results;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        SortMethods metodos = new SortMethods();
+        int tamanio = 1000;
+        int[] array = generate(tamanio);
+        int[] arrayBubble = Arrays.copyOf(array, tamanio);
+        int[] arrayBubbleAvz = Arrays.copyOf(array, tamanio);
+
+        Callable<Void> miFuncionBubble = () -> {
+            metodos.sortBubble(arrayBubble);
+            return null;
+        };
+        Callable<Void> miFuncionBubbleAvz = () -> {
+            metodos.sortBubbleAvan(arrayBubbleAvz);
+            return null;
+        };
+        Results resultsBubble = BenchMarking.MedirTiempo(miFuncionBubble);
+        System.out.println("Burbuja " + resultsBubble.getTimeResult());
+
+        Results resultsBubbleAvz = BenchMarking.MedirTiempo(miFuncionBubbleAvz);
+        System.out.println("Burbuja Avz " + resultsBubble.getTimeResult());
+    }
+
+    public static int[] generate(int size) {
+
+        int[] numeros = new int[size];
+        for (int i = 0; i < size; i++){
+            numeros[i] = (int) (Math.random() * 10000);
+        }
+        return numeros;
 
         
     }
---
+++ src/BenchMarking.java
@@ -0,0 +1,34 @@
+import java.util.concurrent.Callable;
+
+import javax.management.RuntimeErrorException;
+
+import controllers.*;
+
+import models.Results;
+
+public class BenchMarking {
+
+    public static Results MedirTiempo (Callable<Void> funcion){
+        
+        try {
+        // -CALCULAR TIEMPO INICIO
+            long inicio = System.nanoTime();
+        // -EJECUTAR TIEMPO EJECUCION
+            funcion.call();
+        // -CALCULAR TIEMPO FIN
+            long fin = System.nanoTime();
+        // -CALCULAR TIEMPO EN EJECUCION
+            long duracion = fin - inicio;
+        // -DEVOLVER RESULTADOS 
+            Results results = new Results(duracion);
+            return results;
+        
+        } catch (Exception e) {
+
+            throw new RuntimeException("Error en la funcion");
+            // System.err.println("Error en la funcion");
+            // return null;
+        }
+    }
+    
+}
